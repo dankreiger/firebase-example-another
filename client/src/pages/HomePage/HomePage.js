5c2a3c9cb3c4aa475d258c131f80226b158ba63a
Dan Kreiger <dan@dankreiger.com>
2019-08-21 02:59:47 +0200

    chore(*): remove throw

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -3,13 +3,10 @@ import React from 'react';
 import Directory from '../../components/Directory/Directory';
 import { HomePageWrapper } from './HomePage.styles';
 
-const HomePage = () => {
-  // throw Error;
-  return (
-    <HomePageWrapper>
-      <Directory />
-    </HomePageWrapper>
-  );
-};
+const HomePage = () => (
+  <HomePageWrapper>
+    <Directory />
+  </HomePageWrapper>
+);
 
 export default HomePage;
