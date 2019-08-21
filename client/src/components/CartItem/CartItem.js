8196af6ce2e6284f0b960bee9313b1d323838bdd
Dan Kreiger <dan@dankreiger.com>
2019-08-21 03:05:41 +0200

    refactor(memo): memoize cartItem component

--- client/src/components/CartItem/CartItem.js
+++ client/src/components/CartItem/CartItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import CartItemType from '../../typings/CartItem.type';
 import { CartItemWrapper, CartItemDetailsWrapper } from './CartItem.styles';
 
@@ -20,4 +20,4 @@ CartItem.propTypes = {
   item: CartItemType
 };
 
-export default CartItem;
+export default memo(CartItem);
